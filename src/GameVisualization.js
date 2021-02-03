00706b1eb749b5ce74d449f0a17035c43163f485
Dan <code@haddigan.email>
2021-02-03 17:11:57 -0500

    add more basic styles

--- src/GameVisualization.js
+++ src/GameVisualization.js
@@ -1,12 +1,31 @@
-import { handData } from "./game";
+import { handData, PLAYER_ONE, PLAYER_TWO } from "./game";
+import styles from "./GameVisualization.module.css";
+import { Winner } from "./Winner";
 
-export const GameVisualization = ({ playerHand, opponentHand }) => {
+export const GameVisualization = ({ playerHand, opponentHand, winner }) => {
   return (
     playerHand &&
     opponentHand && (
-      <div>
-        {handData.get(playerHand).label} vs {handData.get(opponentHand).label}
-      </div>
+      <>
+        <div className={styles.gameVisualization}>
+          <Hand isWinner={winner === PLAYER_ONE}>
+            {handData.get(playerHand).label}
+          </Hand>
+          <span>vs.</span>
+          <Hand isWinner={winner === PLAYER_TWO}>
+            {handData.get(opponentHand).label}
+          </Hand>
+        </div>
+        <Winner winner={winner} />
+      </>
     )
+  );
+};
+
+export const Hand = ({ children, isWinner }) => {
+  return (
+    <div className={isWinner ? styles.playedHand : styles.winner}>
+      {children}
+    </div>
   );
 };
--- src/Winner.js
+++ src/Winner.js
@@ -1,3 +1,12 @@
+import { PLAYER_ONE, PLAYER_TWO, DRAW } from "./game";
+import styles from "./Winner.module.css";
+
 export const Winner = ({ winner }) => {
-  return winner ? <div>{winner}</div> : null;
+  return Boolean(winner) ? (
+    <div className={styles.winner}>
+      {winner === DRAW && <div>It's a draw</div>}
+      {winner === PLAYER_ONE && <div>You win!</div>}
+      {winner === PLAYER_TWO && <div>You lose!</div>}
+    </div>
+  ) : null;
 };
